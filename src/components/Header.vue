f8f5f6cd53078b983881a902d806122f28833fe1
liujuwu-China <995386645@qq.com>
2020-11-30 09:08:26 +0800

    111

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header id="header">
+            <h1>{{title}}</h1>
+        </header>        
+    </div>
+</template>
+<script>
+export default {
+    name:"Header",
+    props:{
+        title:String,
+        default:'化学加'
+    }
+}
+</script>
+<style scoped>
+    #header{
+        widows: 100%;
+        height: 50px;
+        background: #fff;
+    }
+    #header h1{
+        font-size: 18px;
+        text-align: center;
+        line
+    }
+</style>
